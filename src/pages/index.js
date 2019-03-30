70d9a51fd4cd9eaf44ad8ed0a46b9c4739f546e5
Param-Harrison <paramanantham@live.com>
2019-03-30 21:27:22 +0200

    feat: updated what i teach section

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,27 +30,35 @@ const IndexPage = () => (
     />
     <TeachSection
       title={`What I teach`}
-      description={`In the large JavaScript Ocean, I explored few of the islands. I love to guide through those islands for beginners and professionals`}
+      description={`In the large JavaScript Ocean, I am exploring few of the Islands. On my way, I love to guide through those islands for beginners and professionals`}
       contents={[
         {
-          title: `HTML and CSS`,
-          description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+          title: `HTML, CSS and Beyond`,
+          description: `User interface is an art. HTML5 and CSS3 are the tools to create UI for web. Most web developers start their learning journey with HTML and CSS.`,
         },
         {
-          title: `JavaScript and DOM`,
-          description: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`,
+          title: `JavaScript++`,
+          description: `JavaScript world has grown into an multi-verse. You can create any kind of applications targeting desktop, web and mobile using JS.`,
         },
         {
-          title: `React Ecosystem`,
-          description: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born.`,
+          title: `React and Move Faster`,
+          description: `Ease of development and maintainability is the key for software development. React has been the game changer in development experience. More and more developers are learning react based web and mobile stack to boost up their career opportunities.`,
         },
         {
           title: `GraphQL Ecosystem`,
-          description: `On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire`,
+          description: `GraphQL become buzzword in tech for recent times along with React. I foresee graphql is here to stay for long with strong communities and companies backing it. A nice to have knowledge will become must have skill on every job description in near future.`,
         },
         {
-          title: `CSS-in-JS and Styling`,
-          description: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested`,
+          title: `CSS-in-JS and CSS++`,
+          description: `What CSS in JS? Are you kidding me? No, I am not. HTML is JS and CSS in JS aren't bad anymore. Its solves lot of development problems in recent times and one of the technologies for future along with React and GraphQL`,
+        },
+        {
+          title: `Flirting with Flutter`,
+          description: `Why flutter? Aren't react developers choose react native for mobile development? Well, I strongly believe in Horses for the Courses. Web and mobile frameworks requires different strategies. I like flutter for several reason, but the main one is, It helps me to learn good things from non JavaScript world. After all, Monopoly is bad, no?!`,
+        },
+        {
+          title: `Gatsby Party`,
+          description: `Isn't Gatsby belongs to React ecosystem? Yes it does. But I want to keep a special mention for this awesome framework. Gatsby along with headless CMS will replace a lot of traditional decades old websites. Oh boy, this is just a leaf out of whole Gatsby plant, I can't wait to see when the plants becomes a beautiful garden.`,
         },
       ]}
     />
