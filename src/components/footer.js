7d29f874ff8a9c39b2e9f01213724f4d8f2a8696
Param-Harrison <paramanantham@live.com>
2019-04-10 09:29:50 +0300

    fix: link to RSS

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
 import { SocialIcon } from 'react-social-icons';
 import { Container } from '../components/commonStyles';
 
@@ -77,7 +76,12 @@ const Footer = () => (
         rel="noopener noreferrer"
         url="https://github.com/learnwithparam"
       />
-      <Link to="/rss.xml" alt="Learn with Param's RSS feed">
+      <a
+        href="/rss.xml"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="Learn with Param's RSS feed"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="30"
@@ -86,7 +90,7 @@ const Footer = () => (
         >
           <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3.374 17c-.897 0-1.626-.727-1.626-1.624s.729-1.624 1.626-1.624 1.626.727 1.626 1.624-.729 1.624-1.626 1.624zm3.885 0c-.03-3.022-2.485-5.474-5.511-5.504v-2.406c4.361.03 7.889 3.555 7.92 7.91h-2.409zm4.081 0c-.016-5.297-4.303-9.571-9.592-9.594v-2.406c6.623.023 11.985 5.384 12 12h-2.408z" />
         </svg>
-      </Link>
+      </a>
     </LinksContainer>
   </FooterContainer>
 );
